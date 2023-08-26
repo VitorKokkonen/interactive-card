043859433d9c385f80a6beac88e7f7e9a1b33ecc
Vitor Hugo Kokkonen <117650625+VitorKokkonen@users.noreply.github.com>
2023-08-26 15:48:44 -0300

    Add files via upload

---
+++ JS/dataCards.js
@@ -0,0 +1,71 @@
+function dadosCartao() {
+    const numberInput = document.getElementById('number');
+    const numberCard = document.getElementById('card-number');
+    const number = numberInput.value.replace(/\D/g, '');
+    const formattedNumber = number.replace(/(\d{4})(?=\d)/g, '$1 ');
+
+    if (formattedNumber !== "") {
+        numberInput.value = formattedNumber;
+        numberCard.innerHTML = formattedNumber;
+    } else {
+        numberInput.value = "";
+        numberCard.innerHTML = "0000 0000 0000 0000";
+    }
+
+    const name = document.getElementById('name').value
+    const nameCard = document.getElementById('name-card')
+
+    if (name !== "") {
+        nameCard.innerHTML = name
+    } else {
+        nameCard.innerHTML = "Jane Appleseed"
+    }
+
+    const month = document.querySelector('.month').value
+    const monthCard = document.getElementById('month');
+
+    if (month !== ""){
+        monthCard.innerHTML = month;
+    } else {
+        monthCard.innerHTML = "00"
+    }
+   
+    const year = document.querySelector('.year').value
+    const yearCard = document.getElementById('year');
+
+    if (year !== ""){
+        yearCard.innerHTML = year;
+    } else {
+        yearCard.innerHTML = "00"
+    }
+
+    const cvc = document.getElementById('cvc').value
+    const cvcCard = document.getElementById('cvcCard')
+
+    if(cvc !== ''){
+        cvcCard.innerHTML = cvc
+    } else{
+        cvcCard.innerHTML = '000'
+    }
+
+    buttonEvent()
+
+}
+
+function buttonEvent(){
+    const buttonContinue = document.getElementById('form')
+    const sucess = document.getElementById('sucess')
+    buttonContinue.addEventListener('submit', (e) => {
+        e.preventDefault()
+        buttonContinue.style.display = 'none'
+        sucess.style.display = 'flex'
+
+    })
+    const buttonConfirm = document.getElementById('button-continue')
+    buttonConfirm.addEventListener('click', (e) =>{
+        e.preventDefault()
+        buttonContinue.style.display = 'flex'
+        sucess.style.display = 'none'
+    })
+}
+dadosCartao()
---
+++ JS/validCard.js
@@ -0,0 +1,36 @@
+const errors = [
+    "Wrong Format, numbers only",
+    "Can't be blank",
+    "Invalid month"
+]
+function validaCartao() {
+    const required = document.querySelectorAll('[required]');
+
+    required.forEach(input => {
+        input.addEventListener('invalid', (e) => {
+            e.preventDefault()            
+
+            const elementoMesInvalido = document.querySelector('.month').value;
+            const mes = parseInt(elementoMesInvalido)
+
+            
+            if (input.value == '') {
+                const errorIndex = input.validity.valueMissing ? 1 : 0; // Defina o índice do erro com base na situação
+                const errorMessage = errors[errorIndex];
+
+                const para = document.createElement('p');
+                para.innerHTML = errorMessage;
+
+                input.parentNode.insertBefore(para, input.nextSibling);
+            } else if (mes > 12) {
+                const divDoMes = document.querySelector('.data-experiation')
+                const mesInvalido = document.createElement('p')
+                
+                divDoMes.appendChild(mesInvalido)
+                
+            }
+        });
+    });
+
+}
+validaCartao()
